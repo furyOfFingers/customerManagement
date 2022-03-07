a9aadd23fb00f935398300f5536829ed2fda4a7a
MaratAsadullaev <furyOfFingers@gmail.com>
2022-03-07 14:50:06 +0300

    update react packages

--- frontend/src/pages/Auth/Auth.tsx
+++ frontend/src/pages/Auth/Auth.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { navigate } from "hookrouter";
+import cls from "classnames";
 
 import s from "./Auth.styl";
 
 const Auth = (): JSX.Element => {
   return (
-    <div className={s.container}>
+    <div className={cls(s.container, s.bgRed)}>
       Auth<button onClick={() => navigate("/home")}>to home</button>
     </div>
   );
